d37ff419cadc94715c2d553ad0f1e215c46f5fb7
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-12 13:23:09 -0500

    some basic bulma components for landing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Angular2BulmaModule } from 'angular2-bulma';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
