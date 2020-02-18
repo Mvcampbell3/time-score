f6324102ce8e341248d2e2d0811b0618dc870cd6
Mvcampbell3 <mvcampbell3@gmail.com>
2020-02-18 17:20:51 -0500

    user login route and controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,9 @@
 const db = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
+  // Dev Route
   getAllUsers: (req, res) => {
     db.User.find()
       .then(users => {
@@ -14,7 +16,15 @@ module.exports = {
       });
   },
 
+  // Dev Route
+  deleteAllUsers: (req, res) => {
+    db.User.deleteMany()
+      .then(result => res.json(result))
+      .catch(err => res.json(err))
+  },
+
   createUser: async (req, res) => {
+    // add checks for already existing email and username
     const { username, email, password } = req.body;
     const hash = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS))
 
@@ -31,5 +41,33 @@ module.exports = {
       .catch(err => {
         res.json(err)
       })
+  },
+
+  loginUser: (req, res) => {
+    const { email, password } = req.body;
+    db.User.findOne({ email })
+      .then(async dbUser => {
+        if (!dbUser) {
+          return res.status(401).json({ msg: 'Unauthorized' });
+        }
+
+        const passMatch = await bcrypt.compare(password, dbUser.password);
+        if (passMatch) {
+          // jwt sign and send to client
+          const payload = { email: dbUser.email, username: dbUser.username, id: dbUser._id };
+
+          jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '7d' }, (err, token) => {
+            if (err) {
+              return res.status(500).json({ error: 'Error while creating token' })
+            }
+            return res.status(200).json({ token })
+          })
+        } else {
+          // incorrect pass
+          res.status(401).json({ msg: 'Unauthorized' });
+        }
+      })
+      // res send for db fail
+      .catch(err => res.status(500).json({ error: 'Error while accessing database' }))
   }
 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const user_controller = require('../../controllers/user_controller');
 
+// dev routes except for .post, which is signup 
 router.route('/')
   .get(user_controller.getAllUsers)
   .post(user_controller.createUser)
+  .delete(user_controller.deleteAllUsers)
 
-// router.delete('/delete:id', )
+router.post('/login', user_controller.loginUser)
 
 module.exports = router;
