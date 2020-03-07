4a3e6a822316d0594945e94ea19157afe2356ee9
Mvcampbell3 <mvcampbell3@gmail.com>
2020-03-07 14:21:06 -0500

    game create answers form

--- client/src/app/components/game-create/game-create.component.ts
+++ client/src/app/components/game-create/game-create.component.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class GameCreateComponent implements OnInit {
 
   showInstructions: boolean = false;
-  newAnswersList: { display_value: string, accepted_values: string[] }[] = [{ display_value: "Big Mac", accepted_values: ['Big Mac', 'bigmac'] }];
+  newAnswersList: { display_value: string, accepted_values: string[] }[] = [];
+
+  displayInput: string = "";
+  accepted1: string = "";
+  accepted2: string = "";
+  accepted3: string = "";
+
+  nameInput: string = "";
+  instructionsInput: string = "";
+  descriptionInput: string = "";
 
   constructor() { }
 
@@ -16,9 +25,32 @@ export class GameCreateComponent implements OnInit {
 
   }
 
-
   toggleShowInstructions() {
     this.showInstructions = !this.showInstructions
+  }
+
+  addAnswer() {
+    console.log('clicked')
+    console.log(this.displayInput, this.accepted1, this.accepted2, this.accepted3)
+    if (this.displayInput !== "") {
+      if (this.accepted1 !== "" || this.accepted2 !== "" || this.accepted3 !== "") {
+        const newAnswer = { display_value: "", accepted_values: [] };
+        newAnswer.display_value = this.displayInput;
+        const acceptables = [this.accepted1, this.accepted2, this.accepted3];
+        const filled = acceptables.filter(answer => answer !== "");
+        filled.forEach(one => newAnswer.accepted_values.push(one));
+        console.log(newAnswer)
+        this.newAnswersList.push(newAnswer)
+        this.clearAnswer()
+      }
+    }
+  }
+
+  clearAnswer() {
+    this.displayInput = "";
+    this.accepted1 = "";
+    this.accepted2 = "";
+    this.accepted3 = "";
   }
 
 }
